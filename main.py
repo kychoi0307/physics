004c7ffdd58567d07461bea819dcfc34cb118194
kychoi0307 <kychoi0307@gmail.com>
2026-04-08 16:14:53 +0900

    add chapter 5

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import numpy as np
 
-print(np.pi)
+
+def givenf(x):
+    return x - 2 + np.exp(-x)
+
+
+def dfdx(f, x1, x0):
+    df = f(x1) - f(x0)
+    dx = x1 - x0
+    return df / dx
+
+
+x0 = 3.0
+x1 = 3.5
+tolerance = 1.0e-6
+delta = 1.0
+
+while delta > tolerance:
+    delta = -givenf(x1) / dfdx(givenf, x1, x0)
+    x2 = x1 + delta
+    delta = np.abs(delta)
+    x0, x1 = x1, x2
+
+print("x=%f" % (x1))
